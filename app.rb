f3c571bb29683f7a43f98bfe9263e11ec6128398
Lauren Posey and Olivia Hinton <lauren+olivia@>
2016-07-14 13:39:42 -0700

    updates added to apprb file and views folder updated

--- app.rb
+++ app.rb
@@ -19,15 +19,57 @@ get('/books') do
   erb(:books)
 end
 
+post('/books') do
+  title = params.fetch("title")
+  author = params.fetch("title")
+  book = Book.new({:title => title, :author => author, :id => nil})
+  book.save()
+  @books = Book.all()
+  erb(:books)
+end
+
 get('/patrons') do
   @patrons = Patron.all()
   erb(:patrons)
 end
 
-post('/books')
-  title = params.fetch("title")
-  author = params.fetch("title")
-  book = Book.new({:title => title, :author => author, :id => nil})
+post("/patrons") do
+  name = params.fetch("name")
+  patron = Patron.new({:name => name, :id => nil})
+  patron.save()
+  @patrons = Patron.all()
+  erb(:patrons)
+end
+
+get("/books/:id") do
+  @book = Book.find_book(params.fetch("id"))
+  @patrons = Patron.all()
+  erb(:book_info)
+end
+
+get("/patrons/:id") do
+  @patron = Patron.find_patron(params.fetch("id"))
+  @books = Book.all()
+  erb(:patron_info)
+end
+
+patch("/books/:id") do
+  book_id = params.fetch("id").to_i()
+  @book = Book.find_book(book_id)
+  patron_ids = params.fetch("patron_ids")
+  @book.update({:patron_ids => patron_ids})
+  @patrons = Patron.all()
+  erb(:book_info)
+end
+
+patch("/patrons/:id") do
+  patron_id = params.fetch("id").to_i()
+  @patron = Patron.find_patron(patron_id)
+  book_ids = params.fetch("book_ids")
+  @patron.update({:book_ids => book_ids})
+  @books = Book.all()
+  erb(:patron_info)
+end
 
 # get('/books/new') do
 #   erb(:books_form)
